c8f58feccb7520833b78dce7fe55ce6d3f63082e
ardus-uk <peter@ardus.co.uk>
2022-03-02 15:58:06 +0000

    Intial transfer to git

---
+++ new.js
@@ -0,0 +1,11 @@
+prefix = "./";
+
+arNews = [ "Aiverthwaite<BR>Aughertree<BR>Ireby<BR>WIGTON<BR>Cumbria CA7 1EP","",
+ "Built in Quality and Master Builder of the Year Award Winners","",
+ "We have properties under development currently for sale","property.html",
+  "Renovation and Restoration Specialists","renovation.html",
+    "Barns converted to the highest specifications","renovation.html",
+    "Electrical Installation and Contracting Services","electrical.html",
+       "You can read more about our business<BR>on these pages","company.html",
+     "Please contact us to discuss your requirements","contact.html",
+      "We look forward to receiving<BR>your enquiry.","contact.html" ];
